1e76893d6d2ca6392e56a09a27c58118c0dd5d89
Ryan Mulligan <ryan@ryantm.com>
2021-10-26 10:23:01 -0700

    Revert "zoneminder: 1.34.22 -> 1.36.8"
    This reverts commit 01b6b651b4864e68a44b47c9b9f65a411112be2a.

--- pkgs/servers/zoneminder/default.nix
+++ pkgs/servers/zoneminder/default.nix
@@ -78,14 +78,13 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "zoneminder";
-  version = "1.36.8";
+  version = "1.34.22";
 
   src = fetchFromGitHub {
     owner  = "ZoneMinder";
     repo   = "zoneminder";
     rev    = version;
-    sha256 = "sha256-UUpq4CCZq+EwVNGsLCQuVWdY3yoGw977AaMk1iJ6a5U=";
-    fetchSubmodules = true;
+    sha256 = "1144j9crm0q5pwxnkmy3ahw1vbkddpbk2ys2m2pxxxiqifdhll83";
   };
 
   patches = [
@@ -131,7 +130,7 @@ in stdenv.mkDerivation rec {
 
     for f in scripts/ZoneMinder/lib/ZoneMinder/Config.pm.in \
              scripts/zmupdate.pl.in \
-             src/zm_config_data.h.in \
+             src/zm_config.h.in \
              web/api/app/Config/bootstrap.php.in \
              web/includes/config.php.in ; do
       substituteInPlace $f --replace @ZM_CONFIG_SUBDIR@ /etc/zoneminder
