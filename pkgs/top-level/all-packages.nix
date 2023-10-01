ae0911c3686adc448d0ed2394749da3f4258bdcc
Nico Pulido-Mateo <13149442+npulidomateo@users.noreply.github.com>
2023-10-01 23:30:11 +0200

    typora: reinit at 1.7.5
    typora was removed in bd763298edeb1a7db10e39b1eac205b38a91f9de because
    it failed to run. Running it inside an FHS environment solves the issue.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34259,6 +34259,8 @@ with pkgs;
 
   synfigstudio = callPackage ../applications/graphics/synfigstudio { };
 
+  typora = callPackage ../applications/editors/typora { };
+
   taxi = callPackage ../applications/networking/ftp/taxi { };
 
   taxi-cli = with python3Packages; toPythonApplication taxi;
