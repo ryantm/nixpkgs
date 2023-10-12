bb2c06414c9448f864751abb1556ffb69074141c
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-13 00:55:00 +0200

    luaPackages.toml: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18022,6 +18022,8 @@ with pkgs;
 
   bundler-audit = callPackage ../tools/security/bundler-audit { };
 
+  sol2 = callPackage ../development/libraries/sol2 { };
+
   solargraph = rubyPackages.solargraph;
 
   rbenv = callPackage ../development/ruby-modules/rbenv { };
